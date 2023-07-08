6afa5328e7dc593028ae25e55cae6f2c595891fa
iotadex <wangyi_j@163.com>
2023-07-08 11:33:54 +0800

    finish execute

--- contracts/ibaggregator.sol
+++ contracts/ibaggregator.sol
@@ -3,12 +3,17 @@ pragma solidity 0.8.18;
 
 import "./interfaces/ISwapRouterV2.sol";
 import "./interfaces/ISwapRouterV3.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./libraries/TransferHelper.sol";
 
 contract IbAggregatorRouter {
     /// @notice Thrown when attempting to execute commands and an incorrect number of inputs are provided
     error LengthMismatch();
     /// @notice Thrown when a required command has failed
     error ExecutionFailed(uint256 commandIndex);
+
+    /// @notice Thrown when executing commands with an expired deadline
+    error TransactionDeadlinePassed();
 
     struct SwapRouter {
         uint8 version;
@@ -17,85 +22,99 @@ contract IbAggregatorRouter {
     }
     mapping(bytes1 => SwapRouter) public swapRouters;
 
-    function execute(
-        bytes calldata commands,
+    modifier checkDeadline(uint256 deadline) {
+        if (block.timestamp > deadline) revert TransactionDeadlinePassed();
+        _;
+    }
+
+    function executeEth(
+        address[] calldata tokenIns,
+        uint8[] calldata counts,
         bytes[] calldata inputs,
         uint256 deadline
-    ) external payable {}
+    ) external payable checkDeadline(deadline) {
+        uint256 numTokenIns = tokenIns.length;
+        if (counts.length != numTokenIns) revert LengthMismatch();
 
-    function execute(
-        bytes calldata commands,
-        bytes[] calldata inputs
-    ) public payable {
-        uint256 numCommands = commands.length;
-        if (inputs.length != numCommands) revert LengthMismatch();
+        uint256 amountIn = msg.value;
+        for (uint256 j = 0; j < counts[0]; j++) {
+            bytes calldata input = inputs[j];
+            swap(address(0), amountIn, input);
+        }
 
-        bool success;
-        uint256 outAmount;
-        for (uint256 commandIndex = 0; commandIndex < numCommands; ) {
-            bytes1 command = commands[commandIndex];
-
-            bytes calldata input = inputs[commandIndex];
-
-            //(success, outAmount) = swap(command, input);
-
-            if (!success) {
-                revert ExecutionFailed({commandIndex: commandIndex});
-            }
-
-            unchecked {
-                commandIndex++;
+        uint256 inputIndex = counts[0];
+        for (uint256 i = 1; i < numTokenIns; i++) {
+            amountIn = IERC20(tokenIns[i]).balanceOf(address(this));
+            for (uint256 j = 0; j < counts[i]; j++) {
+                bytes calldata input = inputs[inputIndex];
+                swap(tokenIns[i], amountIn, input);
+                inputIndex++;
             }
         }
     }
 
-    /*
-        address tokenIn;
-        address tokenOut;
-        uint24 fee;
-        address recipient;
-        //uint256 deadline;
-        uint256 amountIn;
-        uint256 amountOutMinimum;
-        uint160 sqrtPriceLimitX96;
-*/
-    function swap(bytes1 platform, bytes calldata inputs) public {
+    function execute(
+        address[] calldata tokenIns,
+        uint8[] calldata counts,
+        bytes[] calldata inputs,
+        uint256 amountIn,
+        uint256 deadline
+    ) external payable checkDeadline(deadline) {
+        uint256 numTokenIns = tokenIns.length;
+        if (counts.length != numTokenIns) revert LengthMismatch();
+        TransferHelper.safeTransferFrom(
+            tokenIns[0],
+            msg.sender,
+            address(this),
+            amountIn
+        );
+        uint256 inputIndex = 0;
+        for (uint256 i = 0; i < numTokenIns; i++) {
+            amountIn = IERC20(tokenIns[i]).balanceOf(address(this));
+            for (uint256 j = 0; j < counts[i]; j++) {
+                bytes calldata input = inputs[inputIndex];
+                swap(tokenIns[i], amountIn, input);
+                inputIndex++;
+            }
+        }
+    }
+
+    function swap(
+        address tokenIn,
+        uint256 amountTotalIn,
+        bytes calldata inputs
+    ) internal returns (uint256) {
+        bytes1 platform;
+        assembly {
+            platform := calldataload(inputs.offset)
+        }
         SwapRouter memory swapRouter = swapRouters[platform];
+
         if (platform > 0x0f) {
-            address tokenIn;
             address tokenOut;
             uint24 fee;
             address recipient;
-            uint256 amountIn;
-            uint256 amountOutMin;
-            uint160 sqrtPriceLimitX96;
+            uint8 percent;
             assembly {
-                tokenIn := calldataload(inputs.offset)
                 tokenOut := calldataload(add(inputs.offset, 0x20))
                 fee := calldataload(add(inputs.offset, 0x40))
                 recipient := calldataload(add(inputs.offset, 0x60))
-                amountIn := calldataload(add(inputs.offset, 0x80))
-                amountOutMin := calldataload(add(inputs.offset, 0xA0))
-                sqrtPriceLimitX96 := calldataload(add(inputs.offset, 0xC0))
+                percent := calldataload(add(inputs.offset, 0x80))
             }
+            return
+                ISwapRouter(swapRouter.router).exactInputSingle(
+                    ISwapRouter.ExactInputSingleParams(
+                        tokenIn == address(0) ? swapRouter.weth : tokenIn,
+                        tokenOut,
+                        fee,
+                        recipient,
+                        999999999999999,
+                        (amountTotalIn * percent) / 100,
+                        0,
+                        0
+                    )
+                );
         } else {}
+        return 0;
     }
-
-    function swapV3(
-        bytes1 platform,
-        ISwapRouter.ExactInputSingleParams calldata params
-    ) public payable {
-        SwapRouter memory swapRouter = swapRouters[platform];
-        //transfer token
-        address tokenIn = params.tokenIn;
-        if (tokenIn == address(0)) {
-            tokenIn = swapRouter.weth;
-            require(msg.value >= params.amountIn, "mismatch eth in");
-        } else {}
-    }
-
-    function swapV2(
-        uint8 platform,
-        ISwapRouter.ExactInputSingleParams calldata params
-    ) public payable {}
 }
